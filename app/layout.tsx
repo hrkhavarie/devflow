3a6404c7cca77c82b10b4d269ad9c93dc36e6150
Hamid Reza Khavarie <hr.khavarie@gmail.com>
2023-10-31 07:48:50 +0330

    add layout config and assets folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,25 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter , Space_Grotesk } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ 
+  subsets: ['latin'],
+  weight:['100' , '200' , '300' , '400' , '500' , '600' , '700' ,'800', '900'],
+  variable: '--font-inter'           
+})
+const spaceGrotesk = Space_Grotesk({ 
+  subsets: ['latin'],
+  weight:[  '300' , '400' , '500' , '600' , '700' ],
+  variable: '--font-spaceGrotesk'           
+})
 
 export const metadata: Metadata = {
   title: 'devsflow',
-  description: 'stackoverflow',
+  description: 'A Community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world.',
+  icons:{
+    icon:'/assets/images/site-logo.svg'
+  }
 }
 
 export default function RootLayout({
@@ -17,8 +29,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <ClerkProvider>
-      <body className={inter.className}>{children}</body>
+      <ClerkProvider 
+        appearance={{
+          elements:{
+            formButtonPrimary:'primary-gradient' , 
+            footerActionLink: 'primary-text-gradient hover:text-primary-500'
+          }
+        }}
+      >
+      <body className={`${inter.variable}         
+        ${spaceGrotesk.variable}`}>
+        {children}</body>
     </ClerkProvider>
     </html>
   )
